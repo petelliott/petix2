9cfe6dd9983379b629a157a561660c3cd3f54163
Peter Elliott <pelliott@ualberta.ca>
2020-07-02 15:23:15 -0600

    Coreutils: added -e and -n to echo

--- bin/coreutils/echo.c
+++ bin/coreutils/echo.c
@@ -1,13 +1,64 @@
 #include <stdio.h>
+#include <unistd.h>
+
+const char escapes[256] = {
+    ['\\'] = '\\',
+    ['a'] = '\a',
+    ['b'] = '\b',
+    ['e'] = '\e',
+    ['f'] = '\f',
+    ['n'] = '\n',
+    ['r'] = '\r',
+    ['t'] = '\t',
+    ['v'] = '\v',
+};
+
+void fputs_escape(const char *s, FILE *f) {
+    for (size_t i = 0; s[i] != '\0'; ++i) {
+        if (s[i] == '\\') {
+            ++i;
+            if (s[i] == '\0' || s[i] == 'c'){
+                break;
+            } else if (escapes[(int)s[i]]) {
+                fputc(escapes[(int)s[i]], stdout);
+            } else {
+                fputc(s[i], stdout);
+            }
+        } else {
+            fputc(s[i], stdout);
+        }
+    }
+}
 
 int main(int argc, char *argv[]) {
-    for (size_t i = 1; i < argc; ++i) {
-        if (i > 1) {
+    bool optn = false;
+    bool opte = false;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "en")) != -1) {
+        if (opt == 'e') {
+            opte = true;
+        } else if (opt == 'n') {
+            optn = true;
+        }
+    }
+
+    for (size_t i = optind; i < argc; ++i) {
+        if (i > optind) {
             fputc(' ', stdout);
         }
-        fputs(argv[i], stdout);
+        if (opte) {
+            fputs_escape(argv[i], stdout);
+        } else {
+            fputs(argv[i], stdout);
+        }
     }
-    fputc('\n', stdout);
+
+    if (!optn) {
+        fputc('\n', stdout);
+    }
+
+    fflush(stdout);
 
     return 0;
 }
