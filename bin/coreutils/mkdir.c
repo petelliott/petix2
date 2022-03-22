6cc0792475a9d7ad108c9f2fbb03da2abb87d2ef
Peter Elliott <pelliott@ualberta.ca>
2022-03-22 01:11:14 -0600

    add mkdir(1)

--- bin/coreutils/mkdir.c
+++ bin/coreutils/mkdir.c
@@ -1,19 +1,17 @@
-#include <fcntl.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        fprintf(stderr, "touch: missing file argument\n");
+        fprintf(stderr, "mkdir: missing directory argument\n");
     }
 
     for (int i = 1; i < argc; ++i) {
-        int fd = open(argv[i], O_CREAT, 0666); // we don't actually support modes lol
-        if (fd == -1) {
+        if (mkdir(argv[i], 0666) == -1) {
             perror("open(2)");
             return 1;
         }
-        close(fd);
     }
     return 0;
 }
