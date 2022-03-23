96143186bd0d03acc5e85091f97ad8c343752ab9
Peter Elliott <pelliott@ualberta.ca>
2022-03-22 20:07:14 -0600

    support O_CREAT (+) in fopen (3)

--- libc/stdio/fopen.c
+++ libc/stdio/fopen.c
@@ -1,11 +1,17 @@
 #include "file.h"
 #include <fcntl.h>
+#include <string.h>
 
 FILE *fopen(const char *path, const char *mode) {
     for (size_t i = 0; i < FOPEN_MAX; ++i) {
         if (!stdio_files[i].valid) {
+            int oflags = 0;
+            if (mode[strlen(mode) - 1] == '+') {
+                oflags |= O_CREAT;
+            }
+
             FILE *f = &(stdio_files[i]);
-            f->fd = open(path, 0);
+            f->fd = open(path, oflags, 0666);
 
             if (f->fd == -1) {
                 return NULL;
